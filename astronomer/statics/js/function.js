36e7eecf9f1848ae31a290d8d39fd0612b46f50e
juampiludu <juanzakka@gmail.com>
2020-05-12 03:43:48 -0300

    fonts changed, about.html modified, send email added

--- astronomer/statics/js/function.js
+++ astronomer/statics/js/function.js
@@ -5,6 +5,10 @@ $(document).ready(function () {
     $('.modal').modal();
     $('.parallax').parallax();
     $('.collapsible').collapsible();
+    $('.materialboxed').materialbox();
+    M.textareaAutoResize($('#emailbody'));
+    $(".mdl-spinner").fadeOut("fast");
+    $(".success-help").fadeOut("fast");
     /*$('.tooltipped').tooltip();*/
     M.updateTextFields();
 
@@ -63,3 +67,42 @@ function enableButton() {
     }
 
 }
+
+$(function () {
+    ["from", "subject", "emailbody"].forEach(function (field) {
+        $("#" + field).attr("required", true);
+        $("#" + field).focus(function () {
+            $(".success-help").fadeOut("fast");
+        });
+    });
+
+    $(".sendEmail").submit(function (e) {
+        $(".mdl-spinner").fadeIn("slow").css("display", "inline-block");
+        e.preventDefault();
+        var from = $("#from").val();
+        var to = 'lu.dev.spprt@gmail.com';
+        var subject = $("#subject").val();
+        var emailbody = $("#emailbody").val();
+        $.ajax({
+            url: "https://mybench-sendingemail.backbench.io/send",
+            data: {
+                from: from,
+                to: to,
+                subject: subject,
+                emailbody: emailbody
+            },
+            error: function () {
+                $(".mdl-spinner").fadeOut("fast");
+                $(".success-help").html("An error has occurred").fadeIn("slow");
+            },
+            success: function (data) {
+                $(".mdl-spinner").fadeOut("fast");
+                ["from", "subject", "emailbody"].forEach(function (field) {
+                    $("#" + field).val("");
+                });
+                $(".success-help").fadeIn("slow");
+            },
+            type: "POST"
+        });
+    });
+});
