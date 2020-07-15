346886fc862d6361511a114b93ef0b33b18b84fd
juampiludu <juanzakka@gmail.com>
2020-07-14 21:29:56 -0300

    change language issue fixed

--- astronomer/astronomerweb/views.py
+++ astronomer/astronomerweb/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, redirect
 from .models import Eclipses
 from adminpanel.models import Years
 from datetime import datetime
+from django.utils.translation import gettext_lazy as _
 
 # Create your views here.
 
 def weight(request):
-    sitename = 'My Weight'
+    sitename = _('My Weight')
     #current_year = datetime.now().year
     return render(request, 'front/weight.html', {'sitename' : sitename})
 
@@ -14,7 +15,7 @@ def nextEclipses(request, year):
     try:
         years = Years.objects.all().order_by('year')
         display = Years.objects.get(year=year)
-        sitename = 'Next Eclipses'
+        sitename = _('Next Eclipses')
         solarEclipse = Eclipses.objects.filter(solar_lunar='Solar').order_by('eclipse_date')
         lunarEclipse = Eclipses.objects.filter(solar_lunar='Lunar').order_by('eclipse_date')
     except:
@@ -23,13 +24,13 @@ def nextEclipses(request, year):
     return render(request, 'front/next_eclipses.html', {'sitename' : sitename, 'solarEclipse' : solarEclipse, 'lunarEclipse' : lunarEclipse, 'years' : years, 'display' : display, 'year' : year})
 
 def about(request):
-    sitename = 'About'
+    sitename = _('About')
     return render(request, 'front/about.html', {'sitename' : sitename})
 
 def mobileapp(request, year):
     years = Years.objects.all().order_by('year')
     display = Years.objects.get(year=year)
-    sitename = 'Next Eclipses'
+    sitename = _('Next Eclipses')
     solarEclipse = Eclipses.objects.filter(solar_lunar='Solar').order_by('eclipse_date')
     lunarEclipse = Eclipses.objects.filter(solar_lunar='Lunar').order_by('eclipse_date')
     return render(request, 'front/mobileapp.html', {'sitename' : sitename, 'solarEclipse' : solarEclipse, 'lunarEclipse' : lunarEclipse, 'years' : years, 'display' : display, 'year' : year})
